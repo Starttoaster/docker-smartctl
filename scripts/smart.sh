6f007f7c19b209a269309400af0bc0e5b7f81455
Brandon Butler <bmbawb@gmail.com>
2019-04-13 07:29:42 -0500

    Updated smartctl one-liner

--- scripts/smart.sh
+++ scripts/smart.sh
@@ -3,5 +3,4 @@
 #Shows S.M.A.R.T. attributes and test results.
 ls /dev/sd* | sed 's/[0-9]//g' | sort -u | while read -r line; do
         echo ============= $line =============
-        smartctl -a $line | grep "Raw_Read\|Power_On\|Power_Cycle\|Reallocated\|Timeout\|Media_Wearout\|CurrentPending\|Uncorrectable"
-        smartctl -l selftest $line | grep "# 1";done
+        smartctl -a /dev/sda | grep "ID#\|Raw_Read\|Power_On\|Power_Cycle\|Reallocated\|Timeout\|Media_Wearout\|CurrentPending\|Uncorrectable\|Status\|# 1";done
