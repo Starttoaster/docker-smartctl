27b119e646e8b560530e6f3e43e7a929639ced6f
Brandon Butler <bmbawb@gmail.com>
2019-04-13 07:33:36 -0500

    Updated script
    Made new lines for separation between drive output

--- scripts/smart.sh
+++ scripts/smart.sh
@@ -2,5 +2,5 @@
 
 #Shows S.M.A.R.T. attributes and test results.
 ls /dev/sd* | sed 's/[0-9]//g' | sort -u | while read -r line; do
-        echo ============= $line =============
+        echo -e "\n============= $line ============="
         smartctl -a /dev/sda | grep "ID#\|Raw_Read\|Power_On\|Power_Cycle\|Reallocated\|Timeout\|Media_Wearout\|CurrentPending\|Uncorrectable\|Status\|# 1";done
