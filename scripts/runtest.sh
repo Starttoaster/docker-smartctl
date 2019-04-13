fc40aa6034ac5e049bcad650de705a41f4bb0393
Brandon Butler <bmbawb@gmail.com>
2019-04-13 07:31:33 -0500

    Updated script

--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -2,4 +2,4 @@
 
 #Starts self-test
 ls /dev/sd* | sed 's/[0-9]//g' | sort -u | while read -r line; do
-        smartctl -t short $line | grep "successful";done
+        smartctl -t short $line | grep "failed";done
