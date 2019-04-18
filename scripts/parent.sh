8b6cd79c4e6f152a9446b20b9c6f63c2dec674c8
Starttoaster <bmbawb@gmail.com>
2019-04-18 03:23:00 -0500

    Fixed several ShellCheck complaints

--- scripts/parent.sh
+++ scripts/parent.sh
@@ -3,8 +3,8 @@
 while true
 do
 	DATE=$(date +"%Y%m%d")
-	./runtest.sh > /logs/$DATE.log
+	./runtest.sh > /logs/"$DATE".log
 	sleep 125s
-	./smart.sh >> /logs/$DATE.log
+	./smart.sh >> /logs/"$DATE".log
 	sleep 86400s
 done
--- scripts/runtest.sh
+++ scripts/runtest.sh
@@ -2,4 +2,4 @@
 
 #Starts self-test
 smartctl --scan | cut -f1 -d" " | while read -r line; do
-        smartctl -q silent -t short $line;done
+        smartctl -q silent -t short "$line";done
